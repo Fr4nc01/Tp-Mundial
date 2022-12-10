db77bad7c8ddee270b1e6138c3010a5e0b9404b9
Franco Lezcano <fr.lezcano08@gmail.com>
2022-12-10 14:05:15 -0300

    BOTONES
    modificación de botones del menú

--- Formulario Qatar 2022/Mundial/Mundial/Menu.Designer.cs
+++ Formulario Qatar 2022/Mundial/Mundial/Menu.Designer.cs
@@ -37,27 +37,29 @@ namespace Mundial
 		private void InitializeComponent()
 		{
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnOctavos = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnCuartos = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnSemi = new System.Windows.Forms.Button();
+            this.btnFinal = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnOctavos
             // 
-            this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.Control;
-            this.button1.Location = new System.Drawing.Point(31, 165);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(356, 63);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "PARTIDOS";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.Button1Click);
+            this.btnOctavos.BackColor = System.Drawing.Color.Transparent;
+            this.btnOctavos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnOctavos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOctavos.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnOctavos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOctavos.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnOctavos.Location = new System.Drawing.Point(31, 165);
+            this.btnOctavos.Name = "btnOctavos";
+            this.btnOctavos.Size = new System.Drawing.Size(356, 63);
+            this.btnOctavos.TabIndex = 0;
+            this.btnOctavos.Text = "OCTAVOS";
+            this.btnOctavos.UseVisualStyleBackColor = false;
+            this.btnOctavos.Click += new System.EventHandler(this.Button1Click);
             // 
             // button2
             // 
@@ -75,21 +77,21 @@ namespace Mundial
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.Button2Click);
             // 
-            // button3
+            // btnCuartos
             // 
-            this.button3.BackColor = System.Drawing.Color.Transparent;
-            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.ForeColor = System.Drawing.SystemColors.Control;
-            this.button3.Location = new System.Drawing.Point(31, 243);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(356, 63);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "POSICION";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.Button3Click);
+            this.btnCuartos.BackColor = System.Drawing.Color.Transparent;
+            this.btnCuartos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnCuartos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCuartos.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCuartos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCuartos.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnCuartos.Location = new System.Drawing.Point(31, 243);
+            this.btnCuartos.Name = "btnCuartos";
+            this.btnCuartos.Size = new System.Drawing.Size(356, 63);
+            this.btnCuartos.TabIndex = 2;
+            this.btnCuartos.Text = "CUARTOS";
+            this.btnCuartos.UseVisualStyleBackColor = false;
+            this.btnCuartos.Click += new System.EventHandler(this.Button3Click);
             // 
             // label1
             // 
@@ -103,17 +105,49 @@ namespace Mundial
             this.label1.Text = "MENU";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // btnSemi
+            // 
+            this.btnSemi.BackColor = System.Drawing.Color.Transparent;
+            this.btnSemi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnSemi.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSemi.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSemi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSemi.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnSemi.Location = new System.Drawing.Point(31, 326);
+            this.btnSemi.Name = "btnSemi";
+            this.btnSemi.Size = new System.Drawing.Size(356, 63);
+            this.btnSemi.TabIndex = 4;
+            this.btnSemi.Text = "SEMI";
+            this.btnSemi.UseVisualStyleBackColor = false;
+            // 
+            // btnFinal
+            // 
+            this.btnFinal.BackColor = System.Drawing.Color.Transparent;
+            this.btnFinal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnFinal.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFinal.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnFinal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFinal.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnFinal.Location = new System.Drawing.Point(31, 411);
+            this.btnFinal.Name = "btnFinal";
+            this.btnFinal.Size = new System.Drawing.Size(356, 63);
+            this.btnFinal.TabIndex = 5;
+            this.btnFinal.Text = "FINAL";
+            this.btnFinal.UseVisualStyleBackColor = false;
+            // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(411, 321);
+            this.ClientSize = new System.Drawing.Size(411, 512);
+            this.Controls.Add(this.btnFinal);
+            this.Controls.Add(this.btnSemi);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnCuartos);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnOctavos);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -125,8 +159,10 @@ namespace Mundial
 
 		}
 		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button btnCuartos;
 		private System.Windows.Forms.Button button2;
-		private System.Windows.Forms.Button button1;
-	}
+		private System.Windows.Forms.Button btnOctavos;
+        private System.Windows.Forms.Button btnSemi;
+        private System.Windows.Forms.Button btnFinal;
+    }
 }
